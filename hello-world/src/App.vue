ce764dc82fb36b6582fcaf77bc87b835a47f8c09
Rafia7 <raf.arnob8@gmail.com>
2021-10-24 19:08:45 -0400

    2

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<h1> Hi </h1>
+ 
 </template>
+<script>
+
+</script>
 
 <style lang="scss">
 #app {
