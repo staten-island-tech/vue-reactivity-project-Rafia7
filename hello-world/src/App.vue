45cfb668f3f1df1e42814fa031e0f32cc9974b17
Rafia7 <raf.arnob8@gmail.com>
2021-10-24 11:48:33 -0400

    2

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -17,16 +17,3 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active _
-      color: #42b983;
-    }
-  }
-}
-</style>
